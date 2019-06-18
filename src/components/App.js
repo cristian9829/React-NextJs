ca57e5a92510e6531b927ca374ed654fd95a5510
Cristian Clavijo <cristian982940@gmail.com>
2019-06-17 23:45:17 -0500

    init project react with using NextJs

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Project React with NextJs</h1>
+    </div>
+  );
+}
+
+export default App;
